a49f8cb7f494f589a147c0c965957c81d4afb02e
TomuraCreator <42077765+TomuraCreator@users.noreply.github.com>
2022-01-13 21:56:12 +0300

    Define configuration db with docker-compose and migration added sql script for migration

---
+++ src/main/resources/migration_script/elama_quiz_create_table.sql
@@ -0,0 +1,34 @@
+﻿CREATE TABLE "quiz" (
+    "ID" serial NOT NULL,
+    "next" integer[] NOT NULL,
+    "prev" integer,
+    "describe" text NOT NULL,
+    "additional" text NOT NULL,
+    "question" text NOT NULL,
+    "delay" int NOT NULL,
+    "final" boolean NOT NULL,
+    CONSTRAINT "pk_quiz" PRIMARY KEY (
+        "ID"
+     )
+);
+
+CREATE TABLE "administrators" (
+    "login" VARCHAR NOT NULL,
+    "password" VARCHAR NOT NULL,
+    "role" VARCHAR(10) NOT NULL,
+    CONSTRAINT "pk_administrators" PRIMARY KEY (
+        "login"
+     )
+);
+
+CREATE TABLE "users" (
+    "email" varchar NOT NULL,
+    "role" VARCHAR(10) NOT NULL,
+    "date_w_tz" timestamp with time zone NOT NULL,
+    "phone_number" VARCHAR(11) NOT NULL,
+    "business_size" VARCHAR NOT NULL,
+    CONSTRAINT "pk_users" PRIMARY KEY (
+        "email"
+     )
+);
+
