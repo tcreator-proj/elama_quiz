3ac229f215245422ef29480d6276931f1eb7766f
Azathoth <vladislavutkin92@gmail.com>
2022-01-26 23:13:33 +0300

    Rewritten liquibase script, connected remote database.

--- src/main/resources/migration_script/elama_quiz_create_table.sql
+++ src/main/resources/migration_script/elama_quiz_create_table.sql
@@ -1,34 +1,35 @@
 ﻿CREATE TABLE "quiz" (
     "ID" serial NOT NULL,
-    "next" integer[] NOT NULL,
+    "next" integer,
+    "group" integer,
     "prev" integer,
-    "describe" text NOT NULL,
-    "additional" text NOT NULL,
-    "question" text NOT NULL,
-    "delay" int NOT NULL,
-    "final" boolean NOT NULL,
+    "describe" text,
+    "additional" text,
+    "content" text,
+    "delay" int,
+    "final" boolean,
+    "isQuestion" boolean,
+    "isAnswer" boolean,
+    "isOption" boolean,
     CONSTRAINT "pk_quiz" PRIMARY KEY (
-        "ID"
-     )
+      "ID"
+    )
 );
 
 CREATE TABLE "administrators" (
-    "login" VARCHAR NOT NULL,
-    "password" VARCHAR NOT NULL,
-    "role" VARCHAR(10) NOT NULL,
-    CONSTRAINT "pk_administrators" PRIMARY KEY (
-        "login"
-     )
+  "login" VARCHAR NOT NULL,
+  "password" VARCHAR NOT NULL,
+  CONSTRAINT "pk_administrators" PRIMARY KEY (
+      "login"
+    )
 );
 
 CREATE TABLE "users" (
-    "email" varchar NOT NULL,
-    "role" VARCHAR(10) NOT NULL,
-    "date_w_tz" timestamp with time zone NOT NULL,
-    "phone_number" VARCHAR(11) NOT NULL,
-    "business_size" VARCHAR NOT NULL,
-    CONSTRAINT "pk_users" PRIMARY KEY (
+     "email" varchar NOT NULL,
+     "date_w_tz" timestamp with time zone NOT NULL,
+     "phone_number" VARCHAR(11) NOT NULL,
+     "business_size" VARCHAR NOT NULL,
+     CONSTRAINT "pk_users" PRIMARY KEY (
         "email"
      )
 );
-
